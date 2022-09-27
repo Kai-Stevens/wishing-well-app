efa65886c6c0ff7b13d760884de20f48b3037aed
Kai-Stevens <kai.stevens97@gmail.com>
2022-09-27 16:47:53 +0100

    Complete styles for main page, and add fetch functionality to display the cards/wishes

---
+++ client/assets/scripts/index.js
@@ -0,0 +1,73 @@
+const getWishData = async () => {
+    try {
+        // Get the data
+        const response = await fetch("http://localhost:3000/wishes");
+        const wishes = await response.json();
+        return wishes;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+const displayWishData = async () => {
+    try {
+        const wishes = await getWishData();
+        console.log(wishes);
+        console.log(wishes[0]);
+        console.log(wishes[0]["name"])
+        //Target the section
+        const wishSection = document.querySelector("#wish-card-section");
+
+        let length = 0;
+        const maxLength = 3
+
+        wishes.length > maxLength ? length = 3 : length = wishes.length; // Max 3 card displays
+
+        for(let i=0; i < length; i++) { // Display the first three wishes
+            //create an html element
+            const wishName = wishes[i]["name"];
+            const wishContent = wishes[i]["wish"];
+            const grantValue = wishes[i]["grant"];
+            const denyValue = wishes[i]["deny"];
+
+            // Create: wish-card (div), name (p), wish(p), grant(div), deny(div)
+            const wishCard = document.createElement('div');
+            wishCard.className = "wish-card";
+
+            const dWishName = document.createElement('p');
+            dWishName.className = "wish-text";
+
+            const dWishContent = document.createElement('p');
+            dWishContent.className = "wish-text";
+
+            const buttonSection = document.createElement('div');
+            buttonSection.className = "wish-buttons";
+
+            const dGrantValue = document.createElement('div');
+            dGrantValue.className = "wish-grant";
+
+            const dDenyValue = document.createElement('div');
+            dDenyValue.className = 'wish-deny';
+
+            // Set content
+            dWishName.textContent = "Name: " + wishName;
+            dWishContent.textContent = "Wish: " + wishContent;
+            dGrantValue.textContent = grantValue;
+            dDenyValue.textContent = denyValue;
+
+            //Append items
+            wishSection.appendChild(wishCard);
+            wishCard.appendChild(dWishName);
+            wishCard.appendChild(dWishContent);
+            wishCard.appendChild(buttonSection);
+
+            buttonSection.appendChild(dGrantValue);
+            buttonSection.appendChild(dDenyValue);
+        }
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+displayWishData();
--- server/wishing-well.js
+++ server/wishing-well.js
@@ -2,16 +2,16 @@ const wishes = [
     {
         name: "Kai",
         wish: "Please can you work.",
-        grant: 0,
-        deny: 0,
+        grant: 5,
+        deny: 2,
         id: 0
 
     },
     {
         name: "App",
         wish: "Please stop trying to make me work.",
-        grant: 0,
-        deny: 0,
+        grant: 2,
+        deny: 1,
         id: 1
     }
 ];
