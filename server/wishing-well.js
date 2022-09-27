798285ec74eb4778b0f4bf23b001362f0cb7154a
Kai-Stevens <kai.stevens97@gmail.com>
2022-09-27 15:31:30 +0100

    Create index page and basic stylings

--- server/wishing-well.js
+++ server/wishing-well.js
@@ -2,11 +2,16 @@ const wishes = [
     {
         name: "Kai",
         wish: "Please can you work.",
+        grant: 0,
+        deny: 0,
         id: 0
+
     },
     {
         name: "App",
         wish: "Please stop trying to make me work.",
+        grant: 0,
+        deny: 0,
         id: 1
     }
 ];
