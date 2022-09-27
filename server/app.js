7398ac17c392c7d64406e3b2cb02c105d18743d6
Kai-Stevens <kai.stevens97@gmail.com>
2022-09-27 14:36:22 +0100

    Add route to handle getting a wish by ID

--- server/app.js
+++ server/app.js
@@ -18,4 +18,22 @@ app.get("/", (req, res) => res.send("Welcome to the all-mighty wishing well"));
 // All of the wishes
 app.get("/wishes", (req, res) => res.send(wishes));
 
+// Specific wish by ID
+app.get("/wishes/:id", (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        if (!id && id !==0) {
+            throw "Invalid input!";
+        } else if (id < 0 || id >= wishes.length) {
+            throw "No such beast!";
+        }
+
+        const filtered = wishes.filter(w => w.id == req.params.id);
+        res.send(filtered[0]);
+    } catch(e) {
+        res.status(404).send({error: e});
+    }
+});
+
 module.exports = app;
